2e7f96bc338b5641fa07b362e3c1f2a164214d11
0ilerr <31261813+0ilerr@users.noreply.github.com>
2021-07-20 19:29:48 -0300

    Project initial task contruction

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,19 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList{
     uint public taskCount = 0;
+
+    struct Task{
+       uint id;
+       string content;
+       bool completed; 
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        
+    }
+
 }
 
 
